80dac6dbbab40265e8bc6af0e64aa5abe363bb11
Jack <stinchjack@gmail.com>
2019-01-11 11:51:37 +1300

    transformation fix

--- src/classes/test.cpp
+++ src/classes/test.cpp
@@ -24,31 +24,31 @@ void testPng() {
    Scene scene;
    scene.useMultiThread = false;
 
-/*
+
     scene.shapes["sphere2"] = SpherePtr();
-    scene.shapes["sphere2"]->transformation.setShift((Point){-1, 0, 0});
-    scene.shapes["sphere2"]->transformation.setScale(1, 1.0 , 1.0);
-    scene.shapes["sphere1"]->transformation.setRotate(Vector(0,0,1), 45);
+    scene.shapes["sphere2"]->transformation.setShift((Point){2, 0, 0});
+    //scene.shapes["sphere2"]->transformation.setScale(1, 1.0 , 1.0);
+    //scene.shapes["sphere2"]->transformation.setRotate(Vector(0,0,1), 45);
     scene.shapes["sphere2"]->diffuse = make_shared<PlainTexture>(Colour (1,1,1));
-*/
+
 
    scene.shapes["sphere1"] = SpherePtr();
-   scene.shapes["sphere1"]->transformation.setShift((Point){2, 0, 0});
+   scene.shapes["sphere1"]->transformation.setShift((Point){1, 0, 0});
    //scene.shapes["sphere1"]->transformation.setScale(1.0, 2.0 , 1.0);
-   scene.shapes["sphere1"]->transformation.setRotate(Vector(0,1,0), 90);
+   //scene.shapes["sphere1"]->transformation.setRotate(Vector(0,0,1), 45);
    scene.shapes["sphere1"]->diffuse = make_shared<PlainTexture>(Colour (.8,0,0));
 
-/*
+
    scene.shapes["sphere3"] = SpherePtr();
    scene.shapes["sphere3"]->transformation.setShift((Point){-2,-2, 8});
-   scene.shapes["sphere1"]->transformation.setScale(3.0, 3, 3);
-   scene.shapes["sphere3"]->diffuse = make_shared<PlainTexture>(Colour (1,1,1));
-*/
+   scene.shapes["sphere3"]->transformation.setScale(3.0, 3, 3);
+   scene.shapes["sphere3"]->diffuse = make_shared<PlainTexture>(Colour (1,.5,1));
+
 
 
    scene.views["view1"] = View (8,8,30);
    scene.views["view1"].setOutput(output);
-   scene.lights["pointlight1"] = make_shared<PointLight>(Colour(1,1,1), (Point){0,0,-30});
+   scene.lights["pointlight1"] = make_shared<PointLight>(Colour(1,1,1), (Point){0,0,-3});
    //scene.views["view1"].setAntiAlias(std::make_shared<SimpleAntiAlias> (SimpleAntiAlias(120)));
 
    //scene.views["view1"].setAntiAlias(
